39f0c1dc00d891a466c029301809e9f27dc94a49
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2021-06-15 17:09:11 -0300

    login

--- appEstadisticas/src/app/components/modal/modal.component.ts
+++ appEstadisticas/src/app/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -8,12 +8,61 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  inputEmail: string = ""
+  inputPassword: string = ""
+
+  constructor(private modalController: ModalController, private menu: MenuController) { }
 
   ngOnInit() {}
 
   dismiss(){
-    console.log("ce3eea")
     this.modalController.dismiss()
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  openMenu(id){
+    this.menu.toggle(id)
+  }
+
+  checkFormSignIn(){
+    if(this.checkEmail() && this.checkPassword()){
+      console.log('papaqpap')
+    }else{
+      console.log('shhshshs')
+    }
+  }
+
+  checkEmail(): Boolean{
+    if(this.inputEmail.length>5 && this.inputEmail.includes('@')){
+      if(this.inputEmail.indexOf('@') > 1 && this.inputEmail.substr(this.inputEmail.indexOf('@')).includes('.')){
+        console.log('sisi')
+        return true
+      }
+    }
+    console.log('nono')
+    return false
+  }
+
+  checkPassword(){
+    let hasNumber = /\d/
+    if(this.inputPassword.length>7 && hasNumber.test(this.inputPassword)){
+      console.log('si')
+      return true
+    }
+    console.log('no')
+    return false
   }
 }
