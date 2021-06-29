8a050f706dac12df38d9f970020b1a3064602f2e
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2021-06-29 14:23:20 -0300

    Update tab1.module.ts

--- appEstadisticas/src/app/tab1/tab1.module.ts
+++ appEstadisticas/src/app/tab1/tab1.module.ts
@@ -6,7 +6,6 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-import { ModalComponent } from '../components/modal/modal.component';
 
 @NgModule({
   imports: [
@@ -16,9 +15,6 @@ import { ModalComponent } from '../components/modal/modal.component';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule,
     FontAwesomeModule
-  ],
-  declarations: [Tab1Page,
-    ModalComponent
   ]
 })
 export class Tab1PageModule {}
