63c03ea032ad957eda72bdd9e3eebf57f186fe4a
GimenezTomas <alumno21.gimenez.tomas@ipm.edu.ar>
2021-09-17 11:03:44 -0300

    asas

--- appEstadisticas/src/app/modals/modal-jugadores/modal-jugadores.page.ts
+++ appEstadisticas/src/app/modals/modal-jugadores/modal-jugadores.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { PartidosService } from 'src/app/services/firebase/partidos.service';
 
 @Component({
   selector: 'app-modal-jugadores',
@@ -8,11 +9,30 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalJugadoresPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private partidosService: PartidosService) { }
 
-  ngOnInit() {}
+  jugadoresSeleccionados: any = []
+  jugadores: any = []
+
+  ngOnInit() {
+    this.getJugadores()
+  }
+
+  getJugadores(){
+    
+  }
 
   dismiss(){
     this.modalController.dismiss()
+  }
+
+  clickJugador(jugador: any){
+    if(!this.jugadoresSeleccionados.includes(jugador) && this.jugadoresSeleccionados.includes(jugador) && this.jugadoresSeleccionados.length<6){
+      this.jugadoresSeleccionados.push(jugador)
+    }else{
+      this.jugadoresSeleccionados.forEach((element: any,index: any)=>{
+        if(element==jugador) this.jugadoresSeleccionados.splice(index,1);
+      });
+    }
   }
 }
