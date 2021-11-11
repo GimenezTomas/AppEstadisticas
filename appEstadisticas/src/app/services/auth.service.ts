d07985243bbaa67a60d886f262cbded46b14e73b
InakiEtc <inaki.etchegoyen@.com>
2021-11-11 15:17:31 -0300

    jj

--- appEstadisticas/src/app/services/auth.service.ts
+++ appEstadisticas/src/app/services/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore,AngularFirestoreDocument } from "@angular/fire/firestore";
 import { Router } from '@angular/router';
-import { Console } from 'console';
 import * as firebase from 'firebase';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
