444ec2b481f4c48422447805ad9145da4f9e4176
Iñaki <inaki.etchegoyen@gmail.com>
2021-11-19 10:37:03 -0300

    juju

--- appEstadisticas/src/app/ver-equipo/jugadores-inicio/jugadores-inicio.page.ts
+++ appEstadisticas/src/app/ver-equipo/jugadores-inicio/jugadores-inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-jugadores-inicio',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JugadoresInicioPage implements OnInit {
 
-  constructor() { }
+  constructor(public authSvc: AuthService) { }
 
   ngOnInit() {
   }
